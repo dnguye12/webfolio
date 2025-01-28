ceb9cf72c90d4adc2c69b911d35b308406cf2158
dnguye12 <duchuyng051@gmail.com>
2025-01-28 07:34:45 +0100

    images

--- data/projects.js
+++ data/projects.js
@@ -2,11 +2,11 @@ const projects = [
     {
       id: 1,
       title: "Portfolio Website",
-      imageUrl: "",
+      imageUrl: "/projects/port.png",
       description: "A personal portfolio website showcasing my technical skills, professional experience, and creative projects in a highly interactive and visually appealing way. The site features advanced animations, clean layouts, and responsive designs to ensure an optimal user experience across devices.",
       summary:
         "This portfolio website serves as a central hub for my professional and personal projects, highlighting my technical skills, design sense, and creativity.",
-      tags: ["React", "TailwindCSS", "ShadCN"],
+      tags: ["React", "TailwindCSS", "ShadCN", "ThreeJS"],
       features: [
         "3D animations",
         "Smooth motions and transitions",
@@ -15,15 +15,15 @@ const projects = [
       techStack: {
         frontend: "React for building dynamic and responsive user interfaces.",
         styling: "TailwindCSS and ShadCN for sleek and user-friendly designs.",
-        hosting: "Deployed on a cloud platform with CI/CD for seamless updates.",
+        "3D": "3D animations and smooth motions.",
       },
-      liveUrl: "https://myportfolio.com",
-      sourceCodeUrl: "https://github.com/username/portfolio-website",
+      liveUrl: "https://webfolio-smoky.vercel.app/",
+      sourceCodeUrl: "https://github.com/dnguye12/webfolio",
     },
     {
       id: 2,
       title: "Dividend Insight (Ongoing)",
-      imageUrl: "",
+      imageUrl: "/projects/div.png",
       description: "A full-stack web application designed to provide investors with real-time company stock information, detailed feedback, price estimations, and comprehensive analysis. The platform integrates AI-powered algorithms, web scraping technology, and interactive data visualizations to deliver actionable insights tailored to user preferences.",
       summary:
         "A comprehensive stock analysis platform that leverages web scraping, data science, and AI to deliver accurate and actionable financial insights.",
@@ -47,7 +47,7 @@ const projects = [
     {
       id: 3,
       title: "Fullstack Miro Copy",
-      imageUrl: "",
+      imageUrl: "/projects/miro.png",
       description: "A real-time collaborative application inspired by Miro, offering teams the ability to visually collaborate using shared whiteboards. The platform includes advanced drawing tools, text editing capabilities, and separate collaboration rooms, making it an essential tool for brainstorming and planning sessions.",
       summary:
         "A highly interactive platform inspired by Miro, designed to enable teams to collaborate visually and in real-time using whiteboards.",
@@ -66,12 +66,12 @@ const projects = [
         hosting: "Deployed on Vercel for fast and reliable access.",
       },
       liveUrl: "https://miro-clone-liard.vercel.app/",
-      sourceCodeUrl: "https://github.com/username/miro-clone",
+      sourceCodeUrl: "https://github.com/dnguye12/Miro-Clone",
     },
     {
       id: 4,
       title: "Fullstack Discord Copy",
-      imageUrl: "",
+      imageUrl: "/projects/discord.png",
       description:
         "A fully functional Discord clone built to offer communities a powerful communication platform with features such as real-time messaging, video and audio calls, and isolated servers or channels. This project mimics the core functionality of Discord with additional improvements in usability and design.",
       summary:
@@ -91,12 +91,12 @@ const projects = [
         hosting: "Deployed on Render for backend and Netlify for frontend.",
       },
       liveUrl: "https://discord-backend-tlyr.onrender.com/",
-      sourceCodeUrl: "https://github.com/username/discord-clone",
+      sourceCodeUrl: "https://github.com/dnguye12/Discord-Backend",
     },
     {
       id: 5,
       title: "Increment Paradise",
-      imageUrl: "",
+      imageUrl: "/projects/para.png",
       description:
         "A specialized e-commerce platform built for a Minecraft server, providing users with the ability to purchase ranks, upgrades, and special in-game items. The platform integrates with the Minecraft server in real-time to ensure a seamless and dynamic shopping experience.",
       summary:
@@ -116,7 +116,7 @@ const projects = [
         hosting: "Deployed on a custom hosting solution with real-time integration to the Minecraft server.",
       },
       liveUrl: "https://shop.incrementalparadise.com/",
-      sourceCodeUrl: "https://github.com/username/increment-paradise",
+      sourceCodeUrl: "",
     },
     {
       id: 6,
